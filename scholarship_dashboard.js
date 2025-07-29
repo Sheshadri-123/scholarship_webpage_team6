f8e410c690db926a511fa91a250bc93d32a9b69c
shesahdri-123 <sheshushesahdri258@gmail.com>
2025-07-29 22:34:09 +0530

    added titles of global scholars program and women in STEM

---
+++ scholarship_dashboard.js
@@ -0,0 +1,12 @@
+const scholarships = [
+  {
+    title: "Global Scholars Program",
+    description: "Full tuition for international students.",
+    category: "Undergraduate"
+  },
+  {
+    title: "Women in STEM",
+    description: "Support for women pursuing STEM degrees.",
+    category: "Postgraduate"
+  },
+
